3906f3332222c5f44ef0b6cf7848f8fa88d22f96
10Pratik01 <using0this@gmail.com>
2025-09-09 00:08:39 +0530

    added layout for app folder

---
+++ frontend-mobile/Sukh/app/_layout.jsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+import './global.css';
+
+export default function RootLayout() {
+  return <Stack>
+   
+    {/* <Stack.Screen 
+      name="(tabs)"  ---> folder name 
+      options={{ headerShown: false }} -- options on the folder
+    />
+    <Stack.Screen 
+      name="movies/[id]"
+      options={{ headerShown: false }}
+    /> */}
+
+
+  </Stack>;
+}
